15ef2abfe4d03462d03800099fc41f1bd4f4c67f
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-01-05 16:22:18 +0000

    Fix warn: unused var

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     let i = (0..10).map(|x| x * 2);
     let is_even = |n:&i32| n % 2 == 0;
     let c = c![n for n in i if is_even(n)];
-    let values: Vec<i32> = c.collect();
+    let _: Vec<i32> = c.collect();
 }
